535188189164aeefab1a350df57a648f4f7faa07
Gursimranjot Singh Gill <132660017+gursimrangillnotinsane@users.noreply.github.com>
2024-01-08 16:01:20 -0500

    Update mainapi.py

--- mainapi.py
+++ mainapi.py
@@ -64,7 +64,7 @@ from typing import Optional
 while True :
     
    try:
-      engine= create_engine('mysql+pymysql://arjangillmain:gill12391@p3nlmysql77plsk.secureserver.net:3306/ph16873094761_', pool_recycle=3600)
+      engine= create_engine('', pool_recycle=3600)
 
       sessionlocal= sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -189,9 +189,9 @@ class post_html(BaseModel):
 
 
 
-# SECRET_KEY="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-# ALGORITHM = "HS256"
-# ACCESS_TOKEN_EXPIRE_MINUTES = 1440
+# SECRET_KEY=""
+# ALGORITHM = ""
+# ACCESS_TOKEN_EXPIRE_MINUTES = 
 
 # def create_access_token(data:dict):
 #    to_encode= data.copy()
